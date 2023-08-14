e685187a7703d7d006188fae65924f23abf76f06
Jan200101 <sentrycraft123@gmail.com>
2023-08-14 14:13:19 +0200

    main: add nouveau discrete GPU detection

--- src/discrete-detection.c
+++ src/discrete-detection.c
@@ -4,6 +4,14 @@
 #include <gio/gio.h>
 #include <gudev/gudev.h>
 
+#ifdef HAS_LIBDRM_NOUVEAU
+#include <xf86drm.h>
+#include <nouveau_drm.h>
+#include <nouveau/nvif/ioctl.h>
+#include <nvif/cl0080.h>
+#include <nvif/class.h>
+#endif
+
 #ifdef HAS_LIBDRM_AMDGPU
 #include <amdgpu.h>
 #include <amdgpu_drm.h>
@@ -13,6 +21,105 @@ G_DEFINE_AUTOPTR_CLEANUP_FUNC(amdgpu_device_handle, free);
 
 typedef int handle;
 G_DEFINE_AUTO_CLEANUP_FREE_FUNC(handle, close, -1)
+
+static gboolean
+get_card_is_discrete_nvidia (GUdevDevice *d)
+{
+	g_autoptr(GUdevDevice) parent = NULL;
+	const char* driver;
+
+	parent = g_udev_device_get_parent (d);
+	driver = g_udev_device_get_driver (parent);
+
+	if (g_strcmp0 (driver, "nouveau") != 0)
+		return FALSE;
+
+#ifdef HAS_LIBDRM_NOUVEAU
+	const char *devname;
+	g_auto(handle) fd = -1;
+
+	devname = g_udev_device_get_property (d, "DEVNAME");
+	if (!devname)
+		return FALSE;
+
+	fd = open (devname, O_RDWR);
+	if (fd < 0)
+		return FALSE;
+
+	g_autofree void *device = malloc(352);
+
+	/* Init device */
+	{	
+		struct {
+			struct nvif_ioctl_v0 ioctl;
+			struct nvif_ioctl_new_v0 new;
+			struct nv_device_v0 dev;
+		} init_args = {
+		  .ioctl = {
+			 .object = 0,
+			 .owner = NVIF_IOCTL_V0_OWNER_ANY,
+			 .route = 0x00,
+			 .type = NVIF_IOCTL_V0_NEW,
+			 .version = 0,
+		  },
+		  .new = {
+			 .handle = 0,
+			 .object = (uintptr_t)device,
+			 .oclass = NV_DEVICE,
+			 .route = NVIF_IOCTL_V0_ROUTE_NVIF,
+			 .token = (uintptr_t)device,
+			 .version = 0,
+		  },
+		  .dev = {
+			 .device = ~0ULL,
+		  },
+	   };
+
+		if (drmCommandWrite (fd, DRM_NOUVEAU_NVIF, &init_args, sizeof(init_args)))
+			return FALSE;
+	}
+
+	/* Query device info */
+	struct {
+		struct nvif_ioctl_v0 ioctl;
+		struct nvif_ioctl_mthd_v0 mthd;
+		struct nv_device_info_v0 info;
+	} args = {
+		.ioctl = {
+			.object = (uintptr_t)device,
+			.owner = NVIF_IOCTL_V0_OWNER_ANY,
+			.route = 0x00,
+			.type = NVIF_IOCTL_V0_MTHD,
+			.version = 0,
+		},
+		.mthd = {
+			.method = NV_DEVICE_V0_INFO,
+			.version = 0,
+		},
+		.info = {
+			.version = 0,
+		},
+	};
+
+	if (drmCommandWriteRead (fd, DRM_NOUVEAU_NVIF, &args, sizeof(args)))
+		return FALSE;
+
+
+	switch (args.info.platform)
+	{
+		case NV_DEVICE_INFO_V0_IGP:
+		case NV_DEVICE_INFO_V0_SOC:
+			return FALSE;
+
+		case NV_DEVICE_INFO_V0_PCI:
+		case NV_DEVICE_INFO_V0_AGP:
+		case NV_DEVICE_INFO_V0_PCIE:
+		default:
+			return TRUE;
+	}
+#endif
+	return FALSE;
+}
 
 static gboolean
 get_card_is_discrete_amdgpu (GUdevDevice *d)
@@ -52,5 +159,6 @@ get_card_is_discrete_amdgpu (GUdevDevice *d)
 gboolean
 get_card_is_discrete (GUdevDevice *d)
 {
-	return get_card_is_discrete_amdgpu (d);
+	return get_card_is_discrete_nvidia(d) \
+		|| get_card_is_discrete_amdgpu (d);
 }
